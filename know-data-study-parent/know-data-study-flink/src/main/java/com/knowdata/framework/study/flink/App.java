8ad97f5bb4c4735da5570c7fb43e62b63ee942b4
Johnny-ZTSD <1125418540@qq.com>
2024-02-22 13:45:14 +0800

    [init] project init

--- know-data-study-parent/know-data-study-flink/src/main/java/com/knowdata/framework/study/flink/App.java
+++ know-data-study-parent/know-data-study-flink/src/main/java/com/knowdata/framework/study/flink/App.java
@@ -4,7 +4,8 @@ package com.knowdata.framework.study.flink;
  * Hello world!
  *
  */
-public class App {
+public class App 
+{
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
