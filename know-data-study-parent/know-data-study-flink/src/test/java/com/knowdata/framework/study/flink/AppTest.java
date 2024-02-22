43a78f966b7d7f8762341fe8be09205978956cbe
Johnny-ZTSD <1125418540@qq.com>
2024-02-22 10:49:18 +0800

    [init] project init

---
+++ know-data-study-parent/know-data-study-flink/src/test/java/com/knowdata/framework/study/flink/AppTest.java
@@ -0,0 +1,14 @@
+package com.knowdata.framework.study.flink;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    public void shouldAnswerWithTrue()
+    {
+    }
+}
---
+++ know-data-study-parent/know-data-study-springboot/src/main/java/com/knowdata/framework/study/springboot/App.java
@@ -0,0 +1,13 @@
+package com.knowdata.framework.study.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
